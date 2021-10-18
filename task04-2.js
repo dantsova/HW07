70b5005942be19aceadc45c9577bc4d14e22d851
Yana <yanadontsv@gmail.com>
2021-10-18 13:57:19 +0300

    Fixed task04-2.js

--- task04-2.js
+++ task04-2.js
@@ -1,15 +1,17 @@
 function highAndLow(str){
     let arr = str.split(' ');
-    let min = arr[0], max = arr[0];
-    for(let i=1; i<arr.length; i++){
-        max = arr[i]>max ?
-            arr[i] : max;
-        min = arr[i]<min ?
-            arr[i] : min;
+    let min = parseInt(arr[0]), max = parseInt(arr[0]);
+    for(let i=1; i<arr.length; ++i){
+        max = parseInt(arr[i])>max ?
+            parseInt(arr[i]) : max;
+        min = parseInt(arr[i])<min ?
+            parseInt(arr[i]) : min;
     }
     return [max, min].join(" ");
 }
 
+
 console.log(highAndLow("1 2 3 4 5"));
 console.log(highAndLow("1 2 -3 4 5"));
 console.log(highAndLow("1 9 3 4 -5"));
+console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
